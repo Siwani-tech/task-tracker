839f584b9976481abcf7996120fcda4379dac825
Siwani-tech <shivanisinha564@gmail.com>
2023-08-17 13:37:59 +0530

    commit changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,9 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-    },
-    mode: 'development',
-    devtool: 'source-map',
-    devServer: {
-        contentBase: './dist',
-    },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
